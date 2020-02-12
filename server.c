7c428773b744a3b593f299d5bff15e88f29d2729
myounker99 <55459902+myounker99@users.noreply.github.com>
2020-02-12 09:58:23 -0600

    Update server.c

--- server.c
+++ server.c
@@ -3,6 +3,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
 
 #define SERVER_PORT 50421
 #define MAX_PENDING 5
